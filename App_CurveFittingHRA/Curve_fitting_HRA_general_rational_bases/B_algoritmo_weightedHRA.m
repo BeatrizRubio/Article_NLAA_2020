fa0b539631e986a3f4888e1c2199f5d8ce0fdeed
E.Mainar-J.M.Peña-B.Rubio <52950661+BeatrizRubio@users.noreply.github.com>
2021-03-07 08:23:32 +0100

    Add files via upload

---
+++ App_CurveFittingHRA/Curve_fitting_HRA_general_rational_bases/B_algoritmo_weightedHRA.m
@@ -0,0 +1,35 @@
+function [x,y] = B_algoritmo_weightedHRA(w,P,f,g,t)
+
+m=length(P);
+allt=t;
+
+hold on;
+set(gca, 'XColor', 'none', 'YColor', 'none')
+
+
+Q=P;
+d=w;
+curva=zeros(size(allt, 2), 2);
+s=1;
+
+%B-algorithm curve
+for t = allt    
+    id = m+1;
+    for i=1:m-1
+        for j=1:m-i
+            d(i+1,j)=g(t)*d(i,j)+f(t)*d(i,j+1);
+            resp = id-(m+1-i);
+            Q(id,1:2)=(g(t)*d(i,j)/d(i+1,j))*Q(resp,1:2)+(f(t)*d(i,j+1)/d(i+1,j))*Q(resp+1,1:2);
+            id = id+1;
+        end
+    end
+    curva(s,1) = Q(id-1, 1);
+    curva(s,2) = Q(id-1, 2);
+    x(s) = curva(s,1); y(s) = curva(s,2);
+    s=s+1;
+end
+plot(x,y,'o')
+
+end 
+
+  
---
+++ App_CurveFittingHRA/Curve_fitting_HRA_general_rational_bases/QRfuncBDA.m
@@ -0,0 +1,51 @@
+
+function BDA = QRfuncBDA(w,f,g,t,n,l)
+
+    
+omega=func_omega(w,f,g,n);
+
+BDA=zeros(l,n+1);
+
+% 
+% C�lculo de la factorizaci�n de la matriz
+%  COmputation of the multipliers m_{i,j}
+
+
+ for i=2:l
+ 	M=  g ( t( i ) )^(n)  / ( g( t(i-1) ) )^(n+1); 
+	BDA( i,1)=M*g(t (i-1) ); 
+    BDA( i,1)= BDA( i,1)*(omega(t(i-1))/omega(t(i)));  % cambio para racionales
+	for j=2: i-1
+      if (j<=n+1)
+	     M=M* g( t ( i-1) )/  g( t(i) ) *  ( f(t(i) )* g(t( i- j+1) )  -  f( t( i-j+1 ) )*g( t( i) ) ) /  ( f( t( i-1) )*g( t(i- j) )-f( t( i-j) )*g( t(i-1) ) );   
+	     BDA(i,j)= M * g( t (i-j) ); 
+         BDA(i,j)= BDA(i,j)*(omega(t(i-1))/omega(t(i)));   % cambio para racionales
+       end
+	end 
+ end
+
+  %Computation of the pivots p_{i,i}
+   BDA(1,1)=g(t(1))^(n);
+   BDA(1,1)=BDA(1,1)*w(1)/(omega(t(1)));   % cambio para racionales
+   q=1;
+   for  i=2:n+1
+  	q=q* (n-i+2)/(i-1)/g(t(i-1));
+   	aux=1;
+   	for k=1: i-1
+   		aux=aux*   ( f( t(i) ) * g( t( k) ) - f( t(k) )*g( t(i) ) );
+   	end 
+        BDA(i,i)=q*aux*(g(t(i)))^(n-i+1);
+        BDA(i,i)=BDA(i,i)*w(i)/(omega(t(i)));   % cambio para racionales
+    end  
+% % % % Computation of the multipliers tilde m_{i,i}
+
+  for j=1:n	
+     coef= f(t(j))/g(t(j));
+            for i= j+1:n+1 
+    	BDA(j,i )=coef* ( n-i+2) / (i-1);
+        BDA(j,i)=(w(i)/w(i-1))*BDA(j,i);       % cambio para racionales
+    	end
+  end
+
+end
+   
