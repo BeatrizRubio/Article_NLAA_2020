63aa0b623853f77100023bda19fd8e601f3b60de
E.Mainar-J.M.Peña-B.Rubio <52950661+BeatrizRubio@users.noreply.github.com>
2021-03-31 09:56:37 +0200

    Add files via upload

---
+++ App_CurveFittingHRA/Curve_fitting_HRA_general_rational_bases/dibujar.m
@@ -0,0 +1,79 @@
+function errores = dibujar(w, f, g, l, t, bx, by)
+n = size(w,2) - 1;
+format long E
+
+
+%Computation of BDA with HRA
+BDA = QRfuncBDA(w,f,g,t,n,l);
+
+[Q,R]=TNQR(BDA);
+   
+R1 = zeros(n+1,n+1);
+for i=1:n+1
+    for j=1:n+1
+        R1(i,j) = R(i,j);
+    end
+end
+  
+%Control points
+dx=transpose(Q)*transpose(bx);
+
+for i=1:n+1
+    d1x(i)=dx(i);
+end
+
+for i=n+2:l
+     d2x(i)=dx(i);
+end
+
+dy=transpose(Q)*transpose(by);
+
+for i=1:n+1
+    d1y(i)=dy(i);
+end
+
+for i=n+2:l
+    d2y(i)=dy(i);
+end
+
+Solbx=transpose(TNSolve(R1,d1x)); %Coordinate x of control point with HRA
+Solby=transpose(TNSolve(R1,d1y)); %Coordinate y of control point with HRA
+
+P = [Solbx Solby];
+
+plot(bx,by,'+');
+
+%Plot curve
+[xb,yb] = B_algoritmo_weightedHRA(w,P,f,g,t);
+
+%HRA errors
+vax= zeros(1,l);
+vay= zeros(1,l);
+
+for i=1:l
+    vax(i)=abs(bx(i)-xb(i));
+    vay(i)=abs(by(i)-yb(i));
+end 
+
+VAMx=(sum(vax)/l);
+VAMy=(sum(vay)/l);
+errores =[VAMx VAMy];
+
+
+%function [Q,C]=TNQR(B);
+%
+% Given B=BD(A), computes Q and C=BD(R), where A=QR is a QR decomp. of A
+%
+% Written September 29, 2004. Added the computation of Q, June 9, 2007
+% 
+% Copyright (c) 2004 Plamen Koev. See COPYRIGHT.TXT for more details.
+
+% function x=TNSolve(B,b)
+% Solves a TN linear system Ax=b, where B=BD(A).
+%
+% This yields Bjorck-Pereyra type methods for every TN matrix A for which
+% we have B=BD(A). A (and thus B) must be square
+%
+% Plamen Koev, Massachusetts Institute of Technology, December 23, 2005
+%
+% Copyright Plamen Koev, see COPYRIGHT.TXT for details.
---
+++ App_CurveFittingHRA/Curve_fitting_HRA_particular_rational_bases/B_algoritmo_weightedHRA.m
@@ -0,0 +1,43 @@
+function [x,y] = B_algoritmo_weightedHRA(a,b,P,f,g,t)
+
+% Algorithm of evaluation and subdivision presented in Section 5 of
+ %E. Mainar, J.M. Pe\~na, B. Rubio, Evaluation and subdivision algorithms for general 
+%classes of totally positive rational bases, Computer Aided Geometric Design, 
+%Volume 81, 2020, 101900, ISNN 0167-8396.
+
+m=length(P);
+n=size(a,2);
+w=func_pesos(a,b);
+w1=w(n,:);
+allt=t;
+
+hold on;
+set(gca, 'XColor', 'none', 'YColor', 'none')
+
+
+Q=P;
+d=w1;
+curva=zeros(size(allt, 2), 2);
+s=1;
+
+%B-algorithm curve
+for t = allt    
+    id = m+1;
+    for i=1:m-1
+        for j=1:m-i
+            d(i+1,j)=g(t)*d(i,j)+f(t)*d(i,j+1);
+            resp = id-(m+1-i);
+            Q(id,1:2)=(g(t)*d(i,j)/d(i+1,j))*Q(resp,1:2)+(f(t)*d(i,j+1)/d(i+1,j))*Q(resp+1,1:2);
+            id = id+1;
+        end
+    end
+    curva(s,1) = Q(id-1, 1);
+    curva(s,2) = Q(id-1, 2);
+    x(s) = curva(s,1); y(s) = curva(s,2);
+    s=s+1;
+end
+plot(x,y,'o')
+
+end 
+
+  
