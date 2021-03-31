05a14fe1c3d6bed72447a2beee483cc6056a933b
E.Mainar-J.M.Peña-B.Rubio <52950661+BeatrizRubio@users.noreply.github.com>
2021-03-31 09:54:05 +0200

    Delete App_CurveFittingHRA directory

--- App_CurveFittingHRA/Curve_fitting_HRA_general_rational_bases/func_omega.m
+++
@@ -1,8 +0,0 @@
-function omega =func_omega(w,f,g,n)
-syms t;
-
-for i = 1:n+1
-    base(i) = w(i) * nchoosek(n,i-1) *f(t)^(i-1) * g(t)^(n-i+1);
-end
-omega=matlabFunction(sum(base));
-
--- App_CurveFittingHRA/Curve_fitting_HRA_particular_rational_bases/func_omega.m
+++
@@ -1,9 +0,0 @@
-function omega =func_omega(a,b,f,g,n)
-w_n_i=func_pesos(a,b);
-syms t;
-
-for i = 1:n+1
-    base(i) = w_n_i(n,i) * nchoosek(n,i-1) *f(t)^(i-1) * g(t)^(n-i+1);
-end
-omega=matlabFunction(sum(base));
-
